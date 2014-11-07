1552077b25ba039eb6abd49975c5126391d5a12a
Wenbo Lin <lwbshr@gmail.com>
2014-11-07 11:16:46 -0800

    update product view

--- CodeIgniter/application/views/Product_display3.php
+++ CodeIgniter/application/views/Product_display3.php
@@ -54,46 +54,91 @@ function Confirm()
  .num_box a:active{background:#E3E3E3;}
  .num,.num_box a{height:30px;line-height:20px;border-color:#ccc;border-style:solid;color:#333;}
  .num{border-width:1px;text-align:center;text-indent:0px;line-height:15px\9;padding:0;margin-top:10px;width:25px;}
- .J_jia{border-width:5px 5px 5px 5px;border-radius:3px 0 0 3px;}
- .J_jian{border-width:5px 5px 5px 5px;border-radius:0 3px 3px 0;}
+ .J_jia{}
+ .J_jian{}
 </style>
 </head>
 <body>
 <div style="margin-top:50px">
-<ul id="product_line2"  style="list-style-type:none; text-align:center;margin-right:auto;margin-left:auto" data-role="listview">
-		<li>
-        <div id="div1" style="display:none; text-align:center">
+
+<div class="container">
+	<div class="col-sm-6">
+		<div id="div1" style="display:none; text-align:center">
 		<?php
 		 if($productImage==null) 
 		echo "<img border='1px' solid #CCC src='../../../../../../../../../../CodeIgniter/image/product1.png'>";
 		else echo"<img border='1px' solid #CCC src='../../../../../../../../../../CodeIgniter/image/".$productImage."'>";
 		?></div>
-        </li>
-        <li style="text-align:center; font-variant:small-caps; font-size:24px" data-role="list-divider"><?php echo $productName?></li>
-         <li style="visibility:hidden">space</li>
-		<li style="font-size:24px; color:#999"> Price: 
-        <?php 
-		if(isset($specialPrice))
-		{
-			echo "<span style='color:#F60; text-decoration:line-through'>$".$productPrice."</span><li><span style='color:#F00;font-size:30px; font-weight:bold;'>SALE: $".$specialPrice."</span></li>";
-		}
-		else
-		{
-			echo "<span style='color:#F60;'>$".$productPrice."</span><li style='visibility:hidden'>space</li><li style='visibility:hidden'>space</li>";	
-		}
-			?>
-        <li><?php echo $productDescription?></li>
-        <li style="visibility:hidden">space</li> 
-        <li style="font-size:20px;">Qty: 
-        <li class="num_box"><button class="J_jia">&nbsp;&nbsp; + &nbsp;&nbsp;</button><label><input type="text" class="num" name="productQuantity"/></label><button class="J_jian">&nbsp;&nbsp; - &nbsp;&nbsp;</button></li>
-        </li>
-        <li style="visibility:hidden">space</li>
-        <li style="font-size:20px;"><a href='#' onClick="Confirm()">Add to Cart</a></li>
-        <li style="visibility:hidden">space</li>
-        <li style="font-size:20px;"><a href="../../Main_page">Go Back</a></li>
-        <li style="visibility:hidden">space</li>
-        <li style="visibility:hidden">space</li>
-</ul>
+	</div>
+	
+	<!-- display product information -->
+	<div class="col-sm-3">
+		
+		<ul id="product_line2"  style="list-style-type:none; text-align:center;margin-right:auto;margin-left:auto" 
+			data-role="listview">
+		
+				<li style="text-align:center; font-variant:small-caps; font-size:24px" data-role="list-divider">
+					<?php echo "<span style='font-size:30pt; font-weight: bold'>".$productName."</span>" ?>
+				</li>
+				
+				<li style="visibility:hidden">space</li>
+				<li style="font-size:24px; color:#999"> Price: 
+				
+				<?php 
+					if(isset($specialPrice))
+					{
+						echo "<span style='color:#F60; text-decoration:line-through'>$".$productPrice."</span><li><span style='color:#F00;font-size:30px; font-weight:bold;'>SALE: $".$specialPrice."</span></li>";
+					}
+					else
+					{
+						echo "<span style='color:#F60;'>$".$productPrice."</span><li style='visibility:hidden'>space</li><li style='visibility:hidden'>space</li>";	
+					}
+				?>
+				
+				<!--<li><?php echo $productDescription?></li>-->
+				<!--<li style="visibility:hidden">space</li>-->
+				 
+				<li style="font-size:20px;">Qty: 
+					<li class="num_box">
+						<button class="J_jia btn btn-default">
+							&nbsp;&nbsp; + &nbsp;&nbsp;
+						</button>
+					
+						<label>
+							<input type="text" class="num" name="productQuantity"/>
+						</label>
+					
+						<button class="J_jian btn btn-default">
+							&nbsp;&nbsp; - &nbsp;&nbsp;
+						</button>
+					</li>
+				</li>
+				<li style="visibility:hidden">space</li>
+				
+				
+				<li style="font-size:20px;">
+					<button type="button" class="btn btn-default">
+						<a href='#' onClick="Confirm()">Add to Cart</a>
+					</button>
+				</li>
+				<li style="visibility:hidden">space</li>
+				<li style="font-size:20px;">
+					<button type="button" class="btn btn-default">
+						<a href="../../Main_page">Go Back</a>
+					</button>
+				</li>
+				
+				
+				<li style="visibility:hidden">space</li>
+				<li style="visibility:hidden">space</li>
+		
+		</ul>
+		
+	</div>
+	
 </div>
+
+</div>
+
 </body>
 </html>
