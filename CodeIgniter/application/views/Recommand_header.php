db16be7085a628afafad0690ca5e9c3647188ce0
jiasrhappy <jiasrhappy@gmail.com>
2014-11-08 10:39:23 -0800

    recommend

--- CodeIgniter/application/views/Recommand_header.php
+++ CodeIgniter/application/views/Recommand_header.php
@@ -1,2 +1,11 @@
-<h1 style="text-align:center">Guess You Like</h1>
+
 <div name="product" style="width:100%">
+<br><br>
+ 
+<div> <a href="../../../../../../../../../../CodeIgniter/index.php/Cart_controller/myCart" class="btn btn-primary" style="width:10%;margin-left:44%;" role="button">Edit cart</a></div>
+<br>
+<div> <a href="../../../../../../../../../../CodeIgniter/index.php/Main_page" class="btn btn-primary" style="width:10%;margin-left:44%;" role="button">More Motorcycles</a></div>
+<br>
+<br>
+<h1 style="text-align:center">Guess You Like</h1>
+<br>
