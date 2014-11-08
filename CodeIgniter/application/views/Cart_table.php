2a18b8218e51067ee5b58acef4265f7eb5e228f0
dezhihe <dezhihe@usc.edu>
2014-11-07 21:03:06 -0800

    a little modify

--- CodeIgniter/application/views/Cart_table.php
+++ CodeIgniter/application/views/Cart_table.php
@@ -1,8 +1,11 @@
 <?php 
 $dataArray = $this->session->all_userdata();
 $specialPrice=$totalPrice/$productQuantity;
+
 if($specialPrice!=$productPrice)
-	echo '<tr>
+	
+	echo'
+	<tr>
 	<td class="active">'.$productName.'</td>
 	<td class="active">'.$productDescription.'</td>
 	<td class="active">'.$productCategoryName.'</td>
@@ -12,7 +15,7 @@ if($specialPrice!=$productPrice)
 	<td class="active">'.'$'.$totalPrice.'</td>
 	<td class="active"><a href="../../../../../../../../../../CodeIgniter/index.php/Cart_controller/delete/'.$productID.'">Remove</a></td>
 	<td class="active"><a href="../../../../../../../../../../CodeIgniter/index.php/Cart_controller/edit/'.$productID.'">Change</a>
-	<td height="100px" class="active"><img width="20%" height="100%" src="../../../../../../../../../../CodeIgniter/image/'.$productImage.'" width="20%" height="30%"></td>
+	<td height="100px" width="100px" class="active"><img  width="100%" height="100%" src="../../../../../../../../../../CodeIgniter/image/'.$productImage.'" ></td>
 	</td></tr>';
 else
 	echo '<tr>
@@ -24,10 +27,11 @@ else
 	<td class="active">'.'$'.$totalPrice.'</td>
 	<td class="active"><a href="../../../../../../../../../../CodeIgniter/index.php/Cart_controller/delete/'.$productID.'">Remove</a></td>
 	<td class="active"><a href="../../../../../../../../../../CodeIgniter/index.php/Cart_controller/edit/'.$productID.'">Change</a></td>
-	<td class="active" width="100px"><img src="../../../../../../../../../../CodeIgniter/image/'.$productImage.'" width="20%" height="30%"></td>
+	<td class="active"  h="100px" width="100px"><img onclick="pic()" width="100%" height="100%" src="../../../../../../../../../../CodeIgniter/image/'.$productImage.'"></td>
 	</tr>';
 
 $sum=$this->session->userdata('sum');
 $sum+=$totalPrice;
 $this->session->set_userdata('sum', $sum);
+
 ?>
--- CodeIgniter/application/views/Header.php
+++ CodeIgniter/application/views/Header.php
@@ -13,7 +13,8 @@ if(!$this->session->userdata('username'))
 	
 	<div class='navbar-form navbar-right'>
 	<div style='position:relative; right:100px'>
-	<a data-role='button' class='btn btn-primary' data-inline='true' href='../../../../../../../../../../CodeIgniter/index.php/Main_page/login'>Log in</a>&nbsp;<a href='../../../../../../../../../../CodeIgniter/index.php/Main_page/register'  class='btn btn-default' data-role='button' data-inline='true'>Register</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div></div></div>";
+	<a data-role='button' class='btn btn-primary' data-inline='true' href='../../../../../../../../../../CodeIgniter/index.php/Main_page/login'>Log in</a>&nbsp;
+	<a href='../../../../../../../../../../CodeIgniter/index.php/Main_page/register'  class='btn btn-default' data-role='button' data-inline='true'>Register</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div></div></div>";
 else
 	echo "
 	 
@@ -29,5 +30,10 @@ else
 	
 	<ul class='navbar-form navbar-right'>
 	<div style='position:relative; right:100px'>
-	<span style=\"color: #FFFFFF\">Welcome, dear ".$dataArray['username']." !&nbsp;&nbsp;</span>&nbsp;<a data-role='button' class='btn btn-primary' href='../../../../../../../../../../CodeIgniter/index.php/Cart_controller/myCart' data-inline='true'>My Cart</a>&nbsp;<a data-inline='true' class='btn btn-success' data-role='button'  href='../../../../../../../../../../CodeIgniter/index.php/Cart_controller/myOrder'>My Order</a>&nbsp;<a data-role='button' data-inline='true' class='btn btn-info' href='../../../../../../../../../../CodeIgniter/index.php/Main_page/profile/".$dataArray['username']."'>My Profile</a>&nbsp;<a data-inline='true' data-role='button' class='btn btn-danger' href='../../../../../../../../../../CodeIgniter/index.php/Main_page/logout'>Log Out</a>&nbsp;</ul></nav></div>";
+	<span style=\"color: #FFFFFF\">Welcome, dear ".$dataArray['username']." !&nbsp;&nbsp;</span>&nbsp;
+	<a data-role='button' class='btn btn-primary' href='../../../../../../../../../../CodeIgniter/index.php/main_page' data-inline='true'>Home</a>&nbsp;
+	<a data-role='button' class='btn btn-primary' href='../../../../../../../../../../CodeIgniter/index.php/Cart_controller/myCart' data-inline='true'>My Cart</a>&nbsp;
+	<a data-inline='true' class='btn btn-success' data-role='button'  href='../../../../../../../../../../CodeIgniter/index.php/Cart_controller/myOrder'>My Order</a>&nbsp;
+	<a data-role='button' data-inline='true' class='btn btn-info' href='../../../../../../../../../../CodeIgniter/index.php/Main_page/profile/".$dataArray['username']."'>My Profile</a>&nbsp;
+	<a data-inline='true' data-role='button' class='btn btn-danger' href='../../../../../../../../../../CodeIgniter/index.php/Main_page/logout'>Log Out</a>&nbsp;</ul></nav></div>";
 ?>
